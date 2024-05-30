d7531ca5cc6a3d04e81afd9ace1a49590ac37a2d
gahyejeon <gahyejeon@gmail.com>
2024-05-30 16:23:40 +0900

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by 내꺼다 on 2022/08/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var lblHello: UILabel!
+    @IBOutlet var txtName: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func btnSend(_ sender: UIButton) {
+        lblHello.text = "Hello, " + txtName.text!
+    }
+    
+}
